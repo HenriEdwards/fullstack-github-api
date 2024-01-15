71007a0bb52ba078c16cf64ae74b633e99cd5a14
Henri E <henriedwards.sa@gmail.com>
2024-01-15 19:37:14 +0200

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    console.log(10);
+  }, []);
   return (
     <div className="App">
       <form>
